88e41ba2f1ebfe9447692d4c6e7bcebe7e6bf41f
Shahnawaz Hossan <s.pabon93@gmail.com>
2020-04-13 21:38:53 +0600

    2d array initialized with object

--- src/screens/Game/index.js
+++ src/screens/Game/index.js
@@ -6,6 +6,34 @@ export default class Game extends React.Component {
 
     constructor(props) {
         super(props);
+
+        let trackMove = [];
+
+        let trackMoveObjects = {
+            direction: "sad",
+            used: true
+        }
+
+        for (let x = 0; x < 8; x++) {
+            trackMove[x] = [];
+            for (let y = 0; y < 8; y++) {
+                trackMove[x][y] = trackMoveObjects;
+            }
+        }
+
+        // var counter = 0;
+
+        // for (let x = 0; x < 8; x++) {
+        //     for (let y = 0; y < 8; y++) {
+        //         console.log(trackMove[x][y]);
+
+        //         counter++;
+        //     }
+        // }
+
+        // console.log("trackMove");
+        // console.log(counter);
+
         this.state = {
             squares: Array(64).fill(null),
             userIsNext: true
